6fcf3a13bd8489788b1704f7407a29cd447e788e
Deivisson Santos <83034488+odeivissonsantos@users.noreply.github.com>
2021-05-14 22:14:24 -0300

    Create Controlador.java

---
+++ Controlador.java
@@ -0,0 +1,49 @@
+public interface Controlador {
+	public static void investimentoTotal() {
+
+	}
+
+	public static void maxVisualizacoes() {
+
+	}
+
+	public static void maxCliques() {
+
+	}
+
+	public static void maxCompartilhamentos() {
+
+	}
+
+	public static void calcula() {
+
+	}
+
+	public static void clientesCadastrados() {
+
+	}
+
+	public static void listarCadastrados() {
+
+	}
+
+	public static void pesquisarCliente() {
+
+	}
+
+	public static void informacoes() {
+
+	}
+
+	public static void pesquisarData() {
+
+	}
+
+	public static void mostraCalc() {
+
+	}
+	public static void cadastrarAnuncio() {
+		
+	}
+
+}
